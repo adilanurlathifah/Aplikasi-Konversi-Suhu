9a83bd5b9b34cf79c0750aca85bff9cd0e89dbfd
Adila Nur Lathifah <strawberrytata3031@gmail.com>
2023-03-30 21:38:07 +0700

    Membuat view binding awal

--- app/src/main/java/org/d3if3143/assesment1/MainActivity.kt
+++ app/src/main/java/org/d3if3143/assesment1/MainActivity.kt
@@ -2,11 +2,30 @@ package org.d3if3143.assesment1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.d3if3143.assesment1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonKonversi.setOnClickListener {
+            konversiSuhu() }
+
+        binding.buttonReset.setOnClickListener {
+            resetKonversi() }
+        }
+
+    private fun resetKonversi() {
+        Log.d("MainActivity", "Tombol diklik!")
     }
+
+    private fun konversiSuhu() {
+        Log.d("MainActivity", "Tombol diklik!")
+    }
+
 }
