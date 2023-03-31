3213e53c353ebc11c227c6b2482de1171f5f8e42
adila <adilanur@student.telkomuniversity.ac.id>
2023-03-31 22:57:59 +0700

    Perbaikan kode dan finishing UI

--- app/src/main/java/org/d3if3143/assesment1/MainActivity.kt
+++ app/src/main/java/org/d3if3143/assesment1/MainActivity.kt
@@ -3,16 +3,22 @@ package org.d3if3143.assesment1
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import org.d3if3143.assesment1.databinding.ActivityMainBinding
 import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var imageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        imageView = binding.imageView
+        imageView.setImageResource(R.drawable._144658)
+
         binding.buttonKonversi.setOnClickListener {
             konversiSuhu() }
         binding.buttonReset.setOnClickListener {
